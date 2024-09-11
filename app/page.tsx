593e5b61dc2ee27287a113a7f95b34bf476e10e5
Johnathanyes <johnathanylee327@gmail.com>
2024-09-11 00:14:39 -0400

    initial setup with clerk and convex authentication.

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { SignInButton, UserButton } from "@clerk/clerk-react";
+import { Unauthenticated, Authenticated } from "convex/react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+      <Authenticated>
+        <UserButton />
+      </Authenticated>
+    </div>
+  );
+}
